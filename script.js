7d64e3a8560e1360a52be7c489f7ce030c8c7540
Cerdtruir <ghoornabeel@gmail.com>
2022-02-24 14:00:32 +0200

    Add script to add book html

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ addBook('ban', 'ban author');
 addBook('apple', 'apple author');
 removeBook('ban');
 
+const booksArray = Object.keys(bookCollection);
+
+document.getElementById('list-of-books');
+
+booksArray.forEach((book) => {
+  removeButton = document.createElement('button');
+  dividerLine = document.createElement('hr');
+
+  removeButton.innerHTML = 'Remove';
+
+  bookSection = document.createElement('div');
+  title = document.createElement('p');
+  author = document.createElement('p');
+
+  title.innerHTML = book;
+  author.innerHTML = bookCollection[book];
+
+  bookSection.append(title, author, removeButton, dividerLine);
+  document.body.querySelector('#list-of-books').append(bookSection);
+});
