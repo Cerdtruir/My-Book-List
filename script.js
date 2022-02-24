d7722a95e95f473de21d5ad83671b1452c5e9c17
Cerdtruir <ghoornabeel@gmail.com>
2022-02-24 13:59:32 +0200

    Add addBook function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 let bookCollection = {};
+
+function addBook(title, author) {
+  bookCollection[title] = author;
+}
+
+addBook('title', 'author');
+addBook('ban', 'ban author');
+addBook('apple', 'apple author');
