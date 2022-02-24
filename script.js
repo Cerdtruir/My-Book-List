df5026afe3a6cac6046fd1ce89a0c78bed954bc7
Cerdtruir <ghoornabeel@gmail.com>
2022-02-24 13:59:56 +0200

    Add removeBook function

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function addBook(title, author) {
   bookCollection[title] = author;
 }
 
+function removeBook(title) {
+  delete bookCollection[title];
+}
+
 addBook('title', 'author');
 addBook('ban', 'ban author');
 addBook('apple', 'apple author');
+removeBook('ban');
+
