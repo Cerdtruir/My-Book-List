df003472fc46a14c3f1ec47a66c8fc1a9f64aae4
Cerdtruir <ghoornabeel@gmail.com>
2022-02-26 12:21:37 +0200

    Change code to use a class instead

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
-let bookCollection = {};
-let booksArray = Object.keys(bookCollection);
+class Books {
+  addBook(title, author) {
+    const id = title + author;
+    this[id] = { title, author };
+  }
+
+  remveBook(id) {
+    delete this[id];
+  }
+}
+
+const bookList = new Books();
 
 function storeBooks() {
-  window.localStorage.setItem('books', JSON.stringify(bookCollection));
+  window.localStorage.setItem('books', JSON.stringify(bookList));
 }
 
 function createList() {
-  booksArray = Object.keys(bookCollection);
+  const booksArray = Object.keys(bookList);
   booksArray.forEach((book) => {
     const removeButton = document.createElement('button');
     const dividerLine = document.createElement('hr');
-
+    const id = bookList[book].title + bookList[book].author;
     removeButton.innerHTML = 'Remove';
-    removeButton.id = book;
+    removeButton.id = id;
 
     const bookSection = document.createElement('div');
     const title = document.createElement('p');
     const author = document.createElement('p');
 
-    title.innerHTML = book;
-    author.innerHTML = bookCollection[book];
+    title.innerHTML = bookList[book].title;
+    author.innerHTML = bookList[book].author;
 
     bookSection.append(title, author, removeButton, dividerLine);
     document.body.querySelector('#list-of-books').append(bookSection);
 
-    document.body.querySelector(`#${book}`).addEventListener('click', () => {
+    document.getElementById(id).addEventListener('click', () => {
       // eslint-disable-next-line no-use-before-define
       removeBook(book);
     });
@@ -36,26 +46,26 @@ function updateHTML() {
   createList();
 }
 
-function removeBook(title) {
-  delete bookCollection[title];
-  storeBooks();
-  updateHTML();
-}
-
-if (window.localStorage.getItem('books')) {
-  const storedBooks = window.localStorage.getItem('books');
-  bookCollection = JSON.parse(storedBooks);
-  document.body.querySelector('#list-of-books').innerHTML = '';
-  createList();
-}
-
 // eslint-disable-next-line no-unused-vars
 function addBook() {
   const addTitle = document.getElementById('title').value;
   const addAuthor = document.getElementById('author').value;
+  bookList.addBook(addTitle, addAuthor);
   document.getElementById('form').reset();
-  bookCollection[addTitle] = addAuthor;
   storeBooks();
-  document.body.querySelector('#list-of-books').innerHTML = '';
-  createList();
+  updateHTML();
 }
+
+function removeBook(id) {
+  delete bookList[id];
+  storeBooks();
+  updateHTML();
+}
+
+const storedBooks = window.localStorage.getItem('books');
+const storage = JSON.parse(storedBooks);
+const storageArray = Object.keys(storage);
+storageArray.forEach((book) => {
+  bookList.addBook(storage[book].title, storage[book].author);
+});
+updateHTML();
